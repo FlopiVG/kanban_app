651537ac7d09de7630cf6917f30a00ab3135642a
Flopi <flopivillagarcia@gmail.com>
2016-12-28 20:26:16 +0100

    From notes to kanban

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,56 +1,24 @@
 import AltContainer from 'alt-container';
 import React from 'react';
-import Notes from './Notes.jsx';
-import NoteActions from '../actions/NoteActions';
-import NoteStore from '../stores/NoteStore';
+import Lanes from './Lanes';
+import LaneActions from '../actions/LaneActions';
+import LaneStore from '../stores/LaneStore';
 
 export default class App extends React.Component {
     render() {
         return (
             <div>
-                <button className="add-note" onClick={this.addNote}>+</button>
+                <button className="add-lane" onClick={this.addLane}>+</button>
                 <AltContainer
-                    stores={[NoteStore]}
-                    inject={{notes: () => NoteStore.getState().notes}}>
-                <Notes onEdit={this.editNote} onDelete={this.deleteNote}/>
+                    stores={[LaneStore]}
+                    inject={{lanes: () => LaneStore.getState().lanes || []}}>
+                <Lanes />
                 </AltContainer>
             </div>
         );
     }
 
-    deleteNote = (id, e) => {
-        // Avoid bubbling to edit
-        e.stopPropagation();
-
-        NoteActions.delete(id);
-    };
-
-    // We are using an experimental feature known as property
-    // initializer here. It allow us to bind the method 'this'
-    // to point at our *App* instance
-    //
-    // Alternatively we could 'bind' at 'constructor' ussing
-    // a line, such as this.addNote = this.addNote.bind(this);
-    addNote = () => {
-        // It would be possible to write this in an imperative style.
-        // I.e., through 'this.state.note.push' and then
-        // 'this.setState({notes: this.state.notes})' to commit.
-        //
-        // I tend to favor functional style whenever that make sense.
-        // Even though it might take more code something, I feel
-        // the benefits (easy to reason about, no side effects)
-        // more than make up for it.
-        //
-        // Libraries, such as Immutable.js, go notch further
-        NoteActions.create({task: 'New task'});
-    };
-
-    editNote = (id, task) => {
-        // Don't modify if trying set an empty value
-        if(!task.trim()){
-            return;
-        }
-
-        NoteActions.update({task: task, id: id});
+    addLane() {
+        LaneActions.create({name: 'New lane'});
     }
 }
--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import Note from './note';
+import Editable from './Editable';
 
-export default ({notes, onEdit, onDelete}) => {
+export default ({notes, onValueClick, onEdit, onDelete}) => {
     return (
         <ul className="notes">
             {notes.map(note =>
                 <li className="note" key={note.id}>
-                    <Note task={note.task}
-                          onEdit={onEdit.bind(null, note.id)}
-                          onDelete={onDelete.bind(null, note.id)}/>
+                    <Editable
+                        editing={note.editing}
+                        value={note.task}
+                        onValueClick={onValueClick.bind(null, note.id)}
+                        onEdit={onEdit.bind(null, note.id)}
+                        onDelete={onDelete.bind(null, note.id)}/>
                 </li>
             )}
         </ul>
